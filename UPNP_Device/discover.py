624dd358064f4faed61661453756abb2ab692b43
Kevin Schlosser <drschlosser@hotmail.com>
2019-01-24 11:48:17 -0700

    fixes use of loopback adapter.

--- UPNP_Device/discover.py
+++ UPNP_Device/discover.py
@@ -56,7 +56,7 @@ def discover(timeout=5, log_level=None, search_ips=[], dump=''):
 
     for adapter in ifaddr.get_adapters():
         for adapter_ip in adapter.ips:
-            if isinstance(adapter_ip.ip, tuple):
+            if isinstance(adapter_ip.ip, tuple) or adapter_ip.nice_name == 'lo0':
                 # adapter_ips += [adapter_ip.ip[0]]
                 continue
             else:
